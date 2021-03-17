bc783156bfa4328fc703e4d597989cfd8655038f
Erickson Powell <ericksonpowell@gmail.com>
2021-03-17 16:01:06 -0700

    push for heroku

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.set("views", path.join(__dirname, "views"));
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller");
-app.use("/", routes);
+// const routes = require("./controllers/burgers_controller");
+// app.use("/", routes);
 
-app.listen(app.get("PORT"), function(){
-  console.log(`Server started on port ${app.get("PORT")}`)
+app.get("/", function(req, res){
+  res.render("home");
+})
+
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`)
 });
